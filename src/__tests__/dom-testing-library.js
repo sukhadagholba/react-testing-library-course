e2250cc3d6b232a9a769aef3543c30c1cbd29e7c
Sukhada Gholba <sukhadagholba@gmail.com>
2018-09-19 12:58:59 -0700

    creatted tests using getQueriesForElement from dom-testing-library to check if renders a number input with a label Favorite Number

--- src/__tests__/dom-testing-library.js
+++ src/__tests__/dom-testing-library.js
@@ -4,10 +4,13 @@ import ReactDOM from 'react-dom'
 import {getQueriesForElement} from 'dom-testing-library'
 import {FavoriteNumber} from '../favorite-number'
 
-test('renders a number input with a label "Favorite Number"', () => {
+it('renders a number input with a label "Favorite Number"', () => {
   const div = document.createElement('div')
   ReactDOM.render(<FavoriteNumber />, div)
-  const {getByLabelText} = getQueriesForElement(div)
-  const input = getByLabelText(/favorite number/i)
-  expect(input).toHaveAttribute('type', 'number')
+
+  const components = getQueriesForElement(div)
+  expect(components.getByLabelText('Favorite Number')).toHaveAttribute(
+    'type',
+    'number',
+  )
 })
